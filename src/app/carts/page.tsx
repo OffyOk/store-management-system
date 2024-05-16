1a95c1e8b58ca5ea706a1c2ffb4abfe9359be2f0
OffyOk <ssrivattanasup@gmail.com>
2024-05-17 01:43:43 +0700

    fix fetch data from fakeapi instead mockup for cart page

--- src/app/carts/page.tsx
+++ src/app/carts/page.tsx
@@ -1,25 +1,70 @@
 import { useOrders } from "../hooks/useOrders";
-import { Orders, OrdersProducts } from "../interfaces/orders.type";
+import { Orders, OrdersProducts, OrdersUsers } from "../interfaces/orders.type";
 import { useUsers } from "../hooks/useUsers";
 import { Users } from "../interfaces/users.type";
 import Link from "next/link";
+import { Products } from "../interfaces/products.type";
+import { useProducts } from "../hooks/useProducts";
 
 export default async function CartList() {
   const orders: Orders[] = await useOrders(); // order: [{id:1,userId:A,date:...,products:[{productId:...,quantity:3},...{}]},...,{}]
   const users: Users[] = await useUsers();
   // we want to know who is this userId ,and what product in a cart
   // so we will go in to each order then use UserId to match with id from Users
-  const reOrder = orders.map((order: Orders) =>
-    users.find((u: Users) => u.id === order.userId)
+  const products: Products[] = await useProducts();
+
+  const ordersProducts: OrdersProducts[] = orders.map((order: Orders) => {
+    let totalAmount = 0;
+
+    order.products.forEach((item) => {
+      const product = products.find((p: Products) => p.id === item.productId);
+
+      if (product) {
+        totalAmount += product.price * item.quantity;
+      }
+    });
+
+    return {
+      id: order.id,
+      userId: order.userId,
+      date: order.date,
+      amount: totalAmount,
+    };
+  });
+
+  const ordersUsers: OrdersUsers[] = ordersProducts.map(
+    (order: OrdersProducts) => {
+      const user = users.find((u: Users) => u.id === order.userId);
+      if (!user) {
+        throw new Error("user not found");
+      } else {
+        return {
+          id: order.id,
+          date: order.date.split("T")[0],
+          fullName: user.name.firstname + "" + user.name.lastname,
+          email: user.email,
+          phone: user.phone,
+          address: user.address,
+          amount: order.amount,
+        };
+      }
+    }
   );
 
-  const randomFullNames = [
-    "Olivia Thompson",
-    "Noah Johnson",
-    "Emma Williams",
-    "Liam Smith",
-    "Ava Brown",
-  ];
+  const sortOrdersUsers: OrdersUsers[] = ordersUsers.sort(
+    (a: OrdersUsers, b: OrdersUsers) => {
+      const dateA = new Date(a.date);
+      const dateB = new Date(b.date);
+
+      // First, compare the dates
+      if (dateA.getTime() !== dateB.getTime()) {
+        return dateB.getTime() - dateA.getTime(); // Descending order
+      }
+
+      // If dates are the same, compare by fullName
+      return a.fullName.localeCompare(b.fullName);
+    }
+  );
 
   return (
     <>
@@ -52,7 +97,7 @@ export default async function CartList() {
         <table className="w-full table-auto">
           <thead>
             <tr>
-              <th>DATE & TIME</th>
+              <th>DATE</th>
               <th>FULL NAME</th>
               <th className="hidden lg:block">EMAIL</th>
               <th>PHONE NUMBER</th>
@@ -61,18 +106,16 @@ export default async function CartList() {
             </tr>
           </thead>
           <tbody>
-            {randomFullNames.map((fn) => {
+            {sortOrdersUsers.map((fn) => {
               return (
-                <tr key={fn}>
-                  <td className="text-center">2022-01-01 12:00:00</td>
-                  <td className="text-center">{fn}</td>
-                  <td className="text-center hidden lg:block">
-                    {fn.split(" ").join("")}@store.com
-                  </td>
-                  <td className="text-center">0912345678</td>
-                  <td className="text-center">1000</td>
+                <tr key={fn.id}>
+                  <td className="text-center">{fn.date}</td>
+                  <td className="text-center">{fn.fullName}</td>
+                  <td className="text-center hidden lg:block">{fn.email}</td>
+                  <td className="text-center">{fn.phone}</td>
+                  <td className="text-center">{fn.amount}</td>
                   <td className="text-center">
-                    <Link href="/carts/view/1">View</Link>
+                    <Link href={`/carts/view/${fn.id}`}>View</Link>
                     <button>Update</button>
                     <button>Delete</button>
                   </td>
