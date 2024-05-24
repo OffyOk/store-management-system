e1829f4a87f51d6e6d2325b75355193fbd6397cd
OffyOk <ssrivattanasup@gmail.com>
2024-05-24 18:21:08 +0700

    Fix icon instead text in button

--- src/app/components/button/CreateButton.tsx
+++ src/app/components/button/CreateButton.tsx
@@ -3,7 +3,21 @@ import Link from "next/link";
 export default function CreateButton(refto: { refto: string; page: string }) {
   return (
     <Link href={refto.refto}>
-      <button className="px-4 py-2 bg-blue-600 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+      <button className="flex gap-1 px-3 py-2 bg-blue-600 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 4.5v15m7.5-7.5h-15"
+          />
+        </svg>
         Add {refto.page}
       </button>
     </Link>
--- src/app/components/button/ViewButton.tsx
+++ src/app/components/button/ViewButton.tsx
@@ -3,8 +3,21 @@ import Link from "next/link";
 export default function ViewButton({ refto }: { refto: string }) {
   return (
     <Link href={refto}>
-      <button className="px-4 py-2 bg-blue-600 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
-        View
+      <button className="flex gap-1 px-3 py-2 bg-blue-600 text-white text-base font-medium rounded-md shadow-sm hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="size-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+          />
+        </svg>
       </button>
     </Link>
   );
